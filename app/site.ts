2aa5c0b522187bdd031f5668c32f2a9eeb0b9b82
Krish Gupta <135469703+kkrishguptaa@users.noreply.github.com>
2026-06-28 15:00:18 +0530

    cool stuff

---
+++ app/site.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "Krish Gupta",
+  description:
+    "I live in paradoxes, and wish to live lives as if I'm trying on different clothes. Dedicated to the experiences that are worth telling the tale.",
+};
